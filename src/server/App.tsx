e1579a1bce61eaea0daa995ba631e800ec99cd8b
Bouteiller Alan <bouteiller.alan@gmail.com>
2020-01-01 18:34:30 +0100

    ✨ feat(router): add historical router

--- src/server/App.tsx
+++ src/server/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Router, Route} from 'react-router';
+import history from './history';
+
+const home: React.FunctionComponent = () => {return (<p>Home</p>)};
 
 const App: React.FunctionComponent = () => {
   return (
-    <p>app</p>
+    <Router history={history}>
+      <Route exact path={'/'} component={home} />
+    </Router>
   );
 };
 
