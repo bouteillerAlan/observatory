f00e0dfa2286510dd56e1054be9098f14ff5da55
Bouteiller Alan <bouteiller.alan@gmail.com>
2020-01-07 15:09:20 +0100

    🚧 feat(map): checkApi and get all data

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,11 +1,29 @@
 import React, {useEffect, useState} from 'react';
-
-const _API_URL = process.env.REACT_APP_API_URL;
-const _apiKey = localStorage.getItem('key');
+import history from '../server/history';
 
 const Map: React.FunctionComponent = () => {
-
   const [characters, setCharacters] = useState([]);
+  const [saisons, setSaisons] = useState([]);
+  const [stories, setStories] = useState([]);
+  const [quests, setQuests] = useState([]);
+
+  const _API_URL = process.env.REACT_APP_API_URL;
+  const _apiKey = localStorage.getItem('key');
+  const _lang = localStorage.getItem('lang');
+
+  /**
+   * check the api key stock in the localstorage
+   * @return {void} return nothing, push new page in history
+   */
+  function checkApiKey() {
+    console.log('api', _apiKey);
+    console.log(localStorage);
+    fetch(`${_API_URL}characters?access_token=${_apiKey}`).then((res: any) => {
+      if (res.status !== 200) {
+        history.push('/');
+      }
+    });
+  }
 
   /**
    * get all characters for one user
@@ -18,8 +36,45 @@ const Map: React.FunctionComponent = () => {
     });
   }
 
+  /**
+   * get all saisons
+   * @return {void} set state
+   */
+  function getSaisons() {
+    fetch(`${_API_URL}stories/seasons?ids=all&lang=${_lang}`).then(async (res: any) => {
+      const data = await res.json();
+      setSaisons(data);
+    });
+  }
+
+  /**
+   * get all stories
+   * @return {void} set state
+   */
+  function getStories() {
+    fetch(`${_API_URL}/stories?ids=all&lang=${_lang}`).then(async (res: any) => {
+      const data = await res.json();
+      setStories(data);
+    });
+  }
+
+  /**
+   * get all quests
+   * @return {void} set state
+   */
+  function getQuests() {
+    fetch(`${_API_URL}/quests?ids=all&lang=${_lang}`).then(async (res: any) => {
+      const data = await res.json();
+      setQuests(data);
+    });
+  }
+
   useEffect(() => {
+    checkApiKey();
     getCharacters();
+    getQuests();
+    getSaisons();
+    getStories();
   }, []);
 
   return (
