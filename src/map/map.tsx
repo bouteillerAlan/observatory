0090c2675de0b136a9bd7555815d5f27b7676992
Bouteiller Alan <bouteiller.alan@gmail.com>
2020-01-07 11:56:07 +0100

    🚧 feat(map): get characters

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+const _API_URL = process.env.REACT_APP_API_URL;
+const _apiKey = localStorage.getItem('key');
 
 const Map: React.FunctionComponent = () => {
-  return (<p>Map</p>);
+
+  const [characters, setCharacters] = useState([]);
+
+  /**
+   * get all characters for one user
+   * @return {void} set state
+   */
+  function getCharacters() {
+    fetch(`${_API_URL}characters?access_token=${_apiKey}`).then(async (res: any) => {
+      const data = await res.json();
+      setCharacters(data);
+    });
+  }
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  return (
+    <section className="map">
+      <div className="container row">
+        <div className="col s12">
+          <h1>Map</h1>
+          {characters.length === 0 ? 'loading' : ''}
+          <ul>
+            {characters.map((character: string) => (
+              <li key={character}>{character}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Map;
