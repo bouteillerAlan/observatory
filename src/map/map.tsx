8d7670144ab91565440ff6d1857a7cc4175b2048
Alan Bouteiller <bouteiller.alan@gmail.com>
2020-01-16 19:59:29 +0100

    ✨ feat(loader): add loader for each page

--- src/map/map.tsx
+++ src/map/map.tsx
@@ -32,72 +32,66 @@ const Map: React.FunctionComponent = () => {
   return (
     <section className="map">
       <Nav active={'map'} />
-      <div className="row">
-        <div className="col s12">
-          <div className="container">
-            {loading ?
-              <div className="progress">
-                <div className="indeterminate"></div>
-              </div> :
-              ''}
-          </div>
-          {dataMap && !loading &&
-            <div className="row screen">
-              <table>
-                <thead>
-                  <tr>
-                    <th colSpan={5}>Persona</th>
-                    {Object.entries(dataMap.dataMap).map(([seasonKey, season]: any) => (
-                      <th key={seasonKey} colSpan={Object.keys(season.stories).length}>{season.name}</th>
-                    ))}
-                  </tr>
-                  <tr>
-                    <th>Race</th>
-                    <th>Classe</th>
-                    <th>Name</th>
-                    <th>Level</th>
-                    <th>Order</th>
-                    {Object.entries(dataMap.dataMap).map(([seasonKey, season]: any) => (
-                      Object.entries(season.stories).map(([storyKey, story]: any) => (
-                        <th key={storyKey+seasonKey}>{story.name} {story.races && '- '+story.races[0]}</th>
-                      ))
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {Object.entries(dataMap.charactersData.characterId).map(([key, value]: any) => (
-                    <tr key={key}>
-                      <td>{value.race}</td>
-                      <td>{value.profession}</td>
-                      <td>{key}</td>
-                      <td>{value.level}</td>
-                      <td>{checkGuild(dataMap.charactersData.questsDone[key])}</td>
-                      {Object.entries(dataMap.dataMap).map(([seasonKey, season]: any) => (
-                        Object.entries(season.stories).map(([storyKey, story]: any) => (
-                          <td key={storyKey+seasonKey} className="subTable">
-                            <table>
-                              <tbody>
-                                <tr>
-                                  {Object.entries(story.quests).map(([questKey, quest]: any) => (
-                                    <td key={questKey}
-                                      className={dataMap.charactersData.questsDone[key].includes(Number(questKey)) ? 'bg-green' : 'bg-red'}
-                                    >
-                                    </td>
-                                  ))}
-                                </tr>
-                              </tbody>
-                            </table>
-                          </td>
-                        ))
-                      ))}
-                    </tr>
+      {loading ?
+        <div className="progress">
+          <div className="indeterminate"> </div>
+        </div> :
+        ''}
+      {dataMap && !loading &&
+        <div className="row screen">
+          <table>
+            <thead>
+              <tr>
+                <th colSpan={5}>Persona</th>
+                {Object.entries(dataMap.dataMap).map(([seasonKey, season]: any) => (
+                  <th key={seasonKey} colSpan={Object.keys(season.stories).length}>{season.name}</th>
+                ))}
+              </tr>
+              <tr>
+                <th>Race</th>
+                <th>Classe</th>
+                <th>Name</th>
+                <th>Level</th>
+                <th>Order</th>
+                {Object.entries(dataMap.dataMap).map(([seasonKey, season]: any) => (
+                  Object.entries(season.stories).map(([storyKey, story]: any) => (
+                    <th key={storyKey+seasonKey}>{story.name} {story.races && '- '+story.races[0]}</th>
+                  ))
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(dataMap.charactersData.characterId).map(([key, value]: any) => (
+                <tr key={key}>
+                  <td>{value.race}</td>
+                  <td>{value.profession}</td>
+                  <td>{key}</td>
+                  <td>{value.level}</td>
+                  <td>{checkGuild(dataMap.charactersData.questsDone[key])}</td>
+                  {Object.entries(dataMap.dataMap).map(([seasonKey, season]: any) => (
+                    Object.entries(season.stories).map(([storyKey, story]: any) => (
+                      <td key={storyKey+seasonKey} className="subTable">
+                        <table>
+                          <tbody>
+                            <tr>
+                              {Object.entries(story.quests).map(([questKey, quest]: any) => (
+                                <td key={questKey}
+                                  className={dataMap.charactersData.questsDone[key].includes(Number(questKey)) ? 'bg-green' : 'bg-red'}
+                                >
+                                </td>
+                              ))}
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    ))
                   ))}
-                </tbody>
-              </table>
-            </div>
-          }
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      </div>
+      }
     </section>
   );
 };
