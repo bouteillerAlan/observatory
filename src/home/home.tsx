33e5d7f57c9a69b29e8649938a5b0789096ccc7b
Alan Bouteiller <bouteiller.alan@gmail.com>
2020-04-13 19:11:43 +0200

    :sparkles: feat(check): form in home

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,152 +1,293 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import M from 'materialize-css';
+import logoBus from '../img/logo.png';
+import card from '../img/card.png';
+// eslint-disable-next-line camelcase
+import flag_fr from '../img/france.png';
+// eslint-disable-next-line camelcase
+import flag_uk from '../img/united-kingdom.png';
+import {checkApiKeyRight} from '../request/checkApiKey';
 import history from '../server/history';
-import './home.scss';
+/* eslint-disable react/no-unescaped-entities */
 
-const _API_URL = process.env.REACT_APP_API_URL;
-
-const Home: React.FunctionComponent = () => {
-  const [apiKey, setApiKey] = useState('');
-  const [lang, setLang] = useState('fr');
-  const [gdpr, setGdpr] = useState(false);
-
-  const [gdprError, setGdprError] = useState({message: '', status: false});
-  const [apiKeyError, setApiKeyError] = useState({message: '', status: false});
-
-  const [disabled, setDisabled] = useState(false);
-
-  /**
-   * handle the value of "disabled"
-   * @return {void} return nothing, set state value
-   */
-  function disableSubmit() {
-    setDisabled(gdprError.status || apiKeyError.status);
-  }
-
-  /**
-   * validate the gdpr checkbox
-   * @param {boolean} value the checkbox value you want to validate
-   * @return {boolean} return true if is ok
-   */
-  async function checkGdpr(value: boolean) {
-    if (!value) {
-      setGdprError({message: 'check obligatoire', status: true});
-      return false;
-    } else {
-      setGdprError({message: '', status: false});
-      return true;
-    }
-  }
-
-  /**
-   * validate the api key whit the gw2 api
-   * @param {string} _apiKey the api key you want to test
-   * @return {boolean} return true if is ok
-   */
-  async function checkAccount(_apiKey: any) {
-    fetch(`${_API_URL}account?access_token=${_apiKey}`, {
-      method: 'GET',
-      mode: 'cors',
-    }).then((res: any) => {
-      if (res.status && res.status === 200) {
-        setApiKeyError({message: '', status: false});
-        return true;
-      } else {
-        setApiKeyError({message: 'Api key check failed', status: true});
-        return false;
-      }
-    });
-  }
-
-  /**
-   * validate an api key
-   * @param {string} value the value you want to validate
-   * @return {boolean} return true if is ok
-   */
-  async function checkApiKey(value: string) {
-    if (value === '') {
-      setApiKeyError({message: 'Value must not be null', status: true});
-      return false;
-    } else if (value.match(/[[\]\\&~@^%!:*$€¤£µ_/+°={}`|#²<>]/gm)) {
-      setApiKeyError({message: 'Unauthorized character', status: true});
-      return false;
-    } else if (!value.match(/[A-Z0-9]{8}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{20}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{12}/gm)) {
-      setApiKeyError({message: 'Value is not a api key format', status: true});
-      return false;
-    } else {
-      return checkAccount(value);
-    }
-  }
-
-  /**
-   * control the form
-   * @param {any} target the event.target
-   * @return {void} void set state value
-   */
-  function handleForm(target: any) {
-    if (target.name === 'apiKey') {
-      setApiKey(target.value);
-      checkApiKey(target.value);
-    } else if (target.name === 'lang') {
-      setLang(target.value);
-    } else if (target.name === 'gdpr') {
-      setGdpr(target.checked);
-      checkGdpr(target.checked);
-    }
-  }
-
-  /**
-   * check the form data and
-   * allow to redirect to the next page or not
-   * @return {void} return nothing, redirect to the next page
-   */
-  async function submitForm() {
-    if (checkApiKey(apiKey) && await checkGdpr(gdpr)) {
-      await localStorage.setItem('obsKey', apiKey);
-      await localStorage.setItem('obsLang', lang);
-      history.push('/map');
-    }
-  }
+const Home = () => {
+  const [apiKey, setApiKey] = useState();
+  const [apiKeyError, setApiKeyError] = useState();
+  const [check, setCheck] = useState(false);
+  const [checkError, setCheckError] = useState();
+  const [lang, setLang] = useState('eng');
 
   useEffect(() => {
-    console.log('#');
-    disableSubmit();
-  }, [gdpr, apiKey, apiKeyError, gdprError]);
+    const elems = document.querySelectorAll('select');
+    const options = {};
+    M.FormSelect.init(elems, options);
+  }, [lang]);
+
+  /**
+   * handle the apiKey input
+   * @Param {React.ChangeEvent<HTMLInputElement>} event
+   * @Return {void} set state
+   */
+  function handleApiKey(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    setApiKey(value);
+    if (value === '') {
+      setApiKeyError('Value must not be null');
+    } else if (value.match(/[[\]\\&~@^%!:*$€¤£µ_/+°={}`|#²<>]/gm)) {
+      setApiKeyError('Unauthorized character');
+    } else if (!value.match(/[A-Z0-9]{8}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{20}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{12}/gm)) {
+      setApiKeyError('Value is not a api key format');
+    } else {
+      setApiKeyError(false);
+    }
+  }
+
+  /**
+   * handle the checkbox
+   * @Param {React.ChangeEvent<HTMLInputElement>} event dom event in checkbox
+   * @Return {void} set state
+   */
+  function handleCheck(event: React.ChangeEvent<HTMLInputElement>) {
+    const target = document.getElementById('check') as HTMLInputElement;
+    const value = target?.checked;
+    if (value) {
+      setCheckError(null);
+      setCheck(value);
+    } else {
+      setCheckError('Accept conditions');
+      setCheck(false);
+    }
+  }
+
+  /**
+   * handle the lang select
+   * @param {React.ChangeEvent<HTMLSelectElement>} event dom event in select
+   * @Return {void} set state
+   */
+  function handleSelect(event: React.ChangeEvent<HTMLSelectElement>) {
+    setLang(event.target.value);
+  }
+
+  /**
+   * handle the submit button
+   * @Return {void} set state and redirect
+   */
+  function handleSubmit() {
+    if (!check) {
+      setCheckError('Accept conditions');
+    } else if (apiKey === '') {
+      setApiKeyError('Value must not be null');
+    } else if (apiKey.match(/[[\]\\&~@^%!:*$€¤£µ_/+°={}`|#²<>]/gm)) {
+      setApiKeyError('Unauthorized character');
+    } else if (!apiKey.match(/[A-Z0-9]{8}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{20}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{4}[-]{1}[A-Z0-9]{12}/gm)) {
+      setApiKeyError('Value is not a api key format');
+    } else {
+      // check the api keys
+      checkApiKeyRight(apiKey).then((res) => {
+        if (!res) {
+          setApiKeyError('Invalid key');
+        } else {
+          localStorage.setItem('obsKey', apiKey);
+          localStorage.setItem('obsLang', lang);
+          setApiKeyError(false);
+          history.push('/card');
+        }
+      });
+    }
+  }
+
+  /**
+   * Set lang and go the next div
+   * @Param {string} lang fr | eng
+   * @Return {void} set state
+   */
+  function getTop(lang: string) {
+    setLang(lang);
+  }
+
 
   return (
-    <section className="home">
-      <div className="container row">
-        <div className="col s12">
-          <h1>Welcome to observatory</h1>
+    <section>
+
+      <div className="demo-3">
+        <div>
+
+          {/* head*/}
+          <div className="head full row col s12 valign-wrapper center-align">
+
+            <div className="meteor ma"> </div>
+            <div className="meteor mb"> </div>
+            <div className="meteor mc"> </div>
+            <div className="meteor md"> </div>
+            <div className="meteor me"> </div>
+            <div className="meteor mf"> </div>
+
+            <div className="row title">
+              <h1><span className="yellow-text">O</span>bservatory</h1>
+              <h2>Keep your story in mind</h2>
+            </div>
+
+            <div className="row lig_yellow"> </div>
+
+            <div className="flag">
+              <p>
+                <span>
+                  <a href="#top">
+                    {/* eslint-disable-next-line camelcase */}
+                    <img className="flag_img" src={flag_fr} alt="flag fr" onClick={() => getTop('fr') }/>
+                  </a>
+                </span>
+                <span>
+                  <a href="#top">
+                    {/* eslint-disable-next-line camelcase */}
+                    <img className="flag_img" src={flag_uk} alt="flag uk" onClick={() => getTop('eng') }/>
+                  </a>
+                </span>
+              </p>
+            </div>
+          </div>
+          {/* end head*/}
+
+          <div className="row container valign-wrapper content" id="top">
+            <div className="row">
+              <div className="col s12 m6 img_content">
+                <img src={logoBus} alt="logo du busmagique"/>
+              </div>
+              <div className="col s12 m6">
+                {lang === 'eng' ?
+                  <div>
+                    <h3>What is Observatory</h3>
+                    <p>Hosted by <a href="https://www.lebusmagique.fr/" target="_blank" rel="noopener noreferrer">
+                      Le Bus Magique</a>, Observatory lets you know where each of your characters is in the Guild Wars 2 quest timeline.
+                    </p>
+                  </div> :
+                  <div>
+                    <h3>C'est quoi Observatory</h3>
+                    <p>Hébergé par <a href="https://www.lebusmagique.fr/" target="_blank" rel="noopener noreferrer">
+                      Le Bus Magique</a>, Observatory permet de savoir ou en est chacun de vos personnages dans la chronologie des quêtes de Guild Wars 2.
+                    </p>
+                  </div>
+                }
+              </div>
+            </div>
+          </div>
+
+          <div className="row container valign-wrapper content">
+            <div className="row">
+              <div className="col s12 m6">
+                {lang === 'eng' ?
+                  <div>
+                    <h3>How it works</h3>
+                    <p>Thanks to the <a href={'https://api.guildwars2.com/v2'} target="_blank" rel="noopener noreferrer">API provided by ArenaNet</a> we have sorted Seasons, Stories and Quests in a chronological order as a card deck.</p>
+                    <p>In each card the quest tree follows the chronological order and provides multiple information about the possible choices of each character.</p>
+                  </div> :
+                  <div>
+                    <h3>Comment ça fonctionne</h3>
+                    <p>Grâce à <a href={'https://api.guildwars2.com/v2'} target="_blank" rel="noopener noreferrer">l'api fournie par ArenaNet</a> nous avons trié par ordre chronologique les Saisons, Histoires et Quêtes sous forme de deck de carte.</p>
+                    <p>Dans chaque carte l'arborescence des quêtes suit l'ordre chronologique et fourni de multiples informations sur les choix possibles ou non de chaque personnage.</p>
+                  </div>
+                }
+              </div>
+              <div className="col s12 m6 img_content">
+                <img src={card} alt="card"/>
+              </div>
+            </div>
+          </div>
+
+          <div className="row container valign-wrapper content">
+            <div>
+              <div className="col s12">
+                {lang === 'eng' ?
+                  <>
+                    <div className="col s12 m6">
+                      <h3>Data storage</h3>
+                      <p>This app needs a API key to work (and others informations entered in the form below). It is stored in your browser via <a href={'https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage'} target="_blank" rel="noopener noreferrer">localStorage</a>.</p>
+                      <p>You can delete this cookie at any time via the button "Reset" present at the top right of your screen. </p>
+                      <p>No data is sent to our server. All data displayed in this application is provided by <a href={'https://api.guildwars2.com/v2'} target="_blank" rel="noopener noreferrer">Guild Wars 2 API</a> and localStorage cookie.</p>
+                    </div>
+                    <div className="col s12 m6">
+                      <h3>Information to provide</h3>
+                      <p>You need to provide a key with the following informations : account, characters, progression and pvp.</p>
+                      <p>You can create a key from your <a href={'https://account.arena.net/applications'} target="_blank" rel="noopener noreferrer">ArenaNet account</a>.</p>
+                    </div>
+                  </> :
+                  <>
+                    <div className="col s12 m6">
+                      <h3>Stockage des données</h3>
+                      <p>Cette application à besoin d'une clé API pour fonctionner (ainsi que des informations entrée dans le formulaire ci-dessous). Ces informations sont stockées dans votre navigateur via <a href={'https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage'} target="_blank" rel="noopener noreferrer">localStorage</a>.</p>
+                      <p>Vous pouvez supprimer ce cookie n'importe quand via le bouton "Reset" présent en haut à droite de votre écran.</p>
+                      <p>Aucune donnée n'est envoyée à notre serveur. Toutes les données affichées sont fournis par <a href={'https://api.guildwars2.com/v2'} target="_blank" rel="noopener noreferrer">l'api Guild Wars 2</a> et le cookie localStorage.</p>
+                    </div>
+                    <div className="col s12 m6">
+                      <h3>Informations à fournir</h3>
+                      <p>Il faut fournir une clé api avec les informations suivantes : account, characters, progression et pvp.</p>
+                      <p>Vous pouvez créer une clé api depuis votre <a href={'https://account.arena.net/applications'} target="_blank" rel="noopener noreferrer">compte ArenaNet</a>.</p>
+                    </div>
+                  </>
+                }
+              </div>
+
+              <div className="col s12 api-input">
+                <div className="input-field col s12 l6">
+                  <input id="apiKey" type="text" className={apiKeyError && 'error'} value={apiKey} onChange={(e) => {
+                    handleApiKey(e);
+                  }}/>
+                  <label htmlFor="apiKey">api key</label>
+                  {apiKeyError && <span className="helper-text">{apiKeyError}</span>}
+                </div>
+                <div className="input-field col s12 l6">
+                  <select value={lang} onChange={(e) => {
+                    handleSelect(e);
+                  }}>
+                    <option value="eng">English</option>
+                    <option value="fr">Français</option>
+                  </select>
+                  <label>Language</label>
+                </div>
+                <div className="col s12 l12">
+                  <button className="btn waves-effect waves-light" onClick={() => {
+                    handleSubmit();
+                  }}>Submit
+                    <i className="material-icons right">send</i>
+                  </button>
+                  <p>
+                    <label>
+                      <input type="checkbox" id="check" onChange={(e) => {
+                        handleCheck(e);
+                      }}/>
+                      <span className={checkError && 'error'}>{lang === 'eng' ?
+                        'I accept the registration of my API key and the choice of my display language in the "localStorage" cookie.' :
+                        'J\'accepte l\'enregistrement de ma clé api et du choix de ma langue d\'affichage dans le cookie localStorage'
+                      }</span>
+                    </label>
+                  </p>
+                </div>
+              </div>
+
+              <div className="col s12">
+                {lang === 'eng' ?
+                  <div className="col s12">
+                    <p>
+                      This app is made by <a href="https://alanbouteiller.dev" target="_blank" rel="noopener noreferrer">Alan Bouteiller</a> for <a href="https://www.lebusmagique.fr/" target="_blank" rel="noopener noreferrer">Le Bus Magique</a>.
+                      Do not hesitate to create an <a href="https://github.com/bouteillerAlan/SpyHistory/issues" target="_blank" rel="noopener noreferrer">Issue</a> if you find a bug.
+                      All game images are © 2019 ArenaNet, Inc..<br/>
+                      <small>CC BY-NC-SA Alan Bouteiller</small>
+                    </p>
+                  </div> :
+                  <div className="col s12">
+                    <p>
+                      Cette application a été codée par <a href="https://alanbouteiller.dev" target="_blank" rel="noopener noreferrer">Alan Bouteiller</a> pour <a href="https://www.lebusmagique.fr/" target="_blank" rel="noopener noreferrer">Le Bus Magique</a>.
+                      N'hésiter pas à créer une <a href="https://github.com/bouteillerAlan/SpyHistory/issues" target="_blank" rel="noopener noreferrer">Issue</a> si vous trouvez un bug.
+                      Toutes les images du jeu sont © 2019 ArenaNet, Inc..<br/>
+                      <small>CC BY-NC-SA Alan Bouteiller</small>
+                    </p>
+                  </div>
+                }
+              </div>
+            </div>
+          </div>
         </div>
-        {/* form */}
-        <div className="col s12">
-          <div className="input-field col s12">
-            <input type="text" name="apiKey" id="apiKey" value={apiKey} onChange={(e) => handleForm(e.target)}/>
-            <label htmlFor="apiKey">API Key</label>
-          </div>
-          <div className="input-field col s12">
-            <select name="lang" id="lang" value={lang} onChange={(e) => handleForm(e.target)}>
-              <option value="fr">Français</option>
-              <option value="eng">English</option>
-            </select>
-            <label htmlFor="lang">Language</label>
-          </div>
-          <div className="input-field col s12">
-            <p>
-              <label>
-                <input type="checkbox" name="gdpr" id="gdpr" checked={gdpr} onChange={(e) => handleForm(e.target)}/>
-                <span>J'aceppte d'enregistrer les données fournis dans le cookie.</span>
-              </label>
-            </p>
-          </div>
-          <div className="input-field col s12">
-            <button className="btn waves-effect waves-light" disabled={disabled} onClick={() => submitForm()}>Submit</button>
-          </div>
-          <div className="col s12">
-            {apiKeyError.status && <div className="errorCard"><p>{apiKeyError.message}</p></div>}
-            {gdprError.status && <div className="errorCard"><p>{gdprError.message}</p></div>}
-          </div>
-        </div>
+
       </div>
     </section>
   );
