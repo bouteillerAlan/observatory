a101edd477fdf3ab5d337e1bee9759960d4ef48e
Alan Bouteiller <bouteiller.alan@gmail.com>
2020-04-22 23:02:14 +0200

    :lipstick: style(navbar): add brand

--- src/nav/nav.tsx
+++ src/nav/nav.tsx
@@ -7,25 +7,42 @@ type NavProps = {
 }
 
 const Nav: React.FunctionComponent<NavProps> = (props: NavProps) => {
+
+  /**
+   * handle api key reset
+   * Return {void}
+   */
+  function handleReset(): void {
+    localStorage.removeItem('obsKey');
+    history.push('/');
+  }
+
+
   return (
     <>
       <nav>
         <div className="nav-wrapper">
+
+          <a className="brand-logo hide-on-small-only show-on-medium-and-up large">
+            Observatory
+          </a>
+          {/* Mobile*/}
+          <a className="brand-logo show-on-small hide-on-med-and-up">
+            Obs.
+          </a>
+
           <a href="#empty" data-target="mobile-nav" className="sidenav-trigger">
             <i className="material-icons">menu</i>
           </a>
-          <ul className='left hide-on-med-and-down'>
+          <ul className='right hide-on-med-and-down'>
             <li className={props.active === 'card' ? 'active' : ''}>
               <a href="/card"><i className="material-icons right">storage</i>Card</a>
             </li>
             <li className={props.active === 'map' ? 'active' : ''}>
               <a href="/map"><i className="material-icons right">apps</i>Map</a>
             </li>
-            <li onClick={() => {
-              localStorage.removeItem('obsKey');
-              history.push('/');
-            }}>
-              <a href="#reset"><i className="material-icons right">autorenew</i>Reset API Key</a>
+            <li onClick={() => handleReset()}>
+              <a href="#reset"><i className="material-icons right rotate">autorenew</i>Reset API Key</a>
             </li>
             <li>
               <a href="lebusmagique.fr"><i className="material-icons right">keyboard_return</i>Retour vers LBM</a>
